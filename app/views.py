0bf568654599b10a1a1db86babe8bff070975c89
dyllew3 <digitald83@gmail.com>
2017-11-20 09:40:01 +0000

    Flask webapp designed to allow a github user to see all their repositories and what language it was written in

---
+++ app/views.py
@@ -0,0 +1,35 @@
+from app import app
+
+from github import Github
+from flask import render_template, flash, redirect
+from .forms import GithubForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', 
+                           title='Index')
+G = None
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form =  GithubForm()
+    if form.validate_on_submit():
+        g = Github(form.username.data, form.password.data)
+        global G
+        G = str([str(x.language) + " " + x.full_name for x in g.get_user().get_repos()])
+        #flash('Repos for %s are="%s"' %
+        #      (form.username.data, str([str(x.language) + " " + x.full_name for x in g.get_user().get_repos()])))
+        
+        return redirect('/repos')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
+@app.route('/repos')
+def print_repos():
+    global G
+    if G:
+        a = G
+        G = None
+        return a
+    return ""
