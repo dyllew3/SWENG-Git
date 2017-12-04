2acc30b050b8431a50f1f85868defa516eb9d94b
dyllew3 <digitald83@gmail.com>
2017-12-04 19:29:59 +0000

    Minor improvements

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
 
-from github import Github
+import github
+from github import Github, GithubException
 from flask import render_template, flash, redirect
 from .forms import GithubForm
 
@@ -15,21 +16,28 @@ G = None
 def login():
     form =  GithubForm()
     if form.validate_on_submit():
-        g = Github(form.username.data, form.password.data)
-        global G
-        G = g.get_user().get_repos()
-        #flash('Repos for %s are="%s"' %
-        #      (form.username.data, str([str(x.language) + " " + x.full_name for x in g.get_user().get_repos()])))
+        try :
+            g = Github(form.username.data, form.password.data)
+            global G
+            G = g.get_user().get_repos()
         
-        return redirect('/repos')
+            return redirect('/repos')
+        except  github.BadCredentialsException:
+            flash('Login Details are incorrect')
+            pass
     return render_template('login.html', 
                            title='Sign In',
                            form=form)
 @app.route('/repos')
 def print_repos():
-    global G
-    if G:
-        a = G
-        G = None
-        return render_template('repos.html', repos=a)
+    try:
+        global G
+        if G:
+            a = G
+            G = None
+            return render_template('repos.html', repos=a)
+    except github.BadCredentialsException:
+        flash('Login Details are incorrect')
+    except Exception:
+        flash('An unknown error has occured,  this may be because you have 2 factor authentication on')
     return redirect('/login')
